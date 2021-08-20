a348e5e971a7b31318e1861de9225d5d8b5abe6c
mustafa <yusufkenan2604@gmail.com>
2021-08-20 14:46:41 +0200

    others classes deleted

--- src/test/java/com/FinWorks/pages/DiscussPageBurhan.java
+++
@@ -1,9 +0,0 @@
-package com.FinWorks.pages;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class DiscussPageBurhan extends BasePage {
-    @FindBy(xpath = "//span[.='Inbox']")
-    public WebElement inboxTab;
-}
